d715181bbea6fb3e0e9ab8cb094542d3624762c7
Tim Hawes <tim.hawes@inin.com>
2015-12-29 19:22:21 -0500

    .

--- src/main/java/com/inin/purecloud/devops/App.java
+++ src/main/java/com/inin/purecloud/devops/App.java
@@ -1,16 +1,33 @@
 package com.inin.purecloud.devops;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        DevopsProducer k = new DevopsProducer("kafka-1,kafka-2,kafka-3");
-        System.out.println( "Sending data..." );
+import java.util.*;
 
-        System.out.println( "Hello World!" );
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+public class App {
+    public static void main(String[] args) {
+        long events = Long.parseLong(args[0]);
+        Random rnd = new Random();
+
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "kafka-1:9092,kafka-2:9092,kafka-3 ");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        props.put("partitioner.class", "com.inin.purecloud.devops.SimplePartitioner");
+        props.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(props);
+
+        Producer<String, String> producer = new Producer<String, String>(config);
+
+        for (long nEvents = 0; nEvents < events; nEvents++) {
+            long runtime = new Date().getTime();
+            String ip = "192.168.2." + rnd.nextInt(255);
+            String msg = runtime + ",www.example.com," + ip;
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
+            producer.send(data);
+        }
+        producer.close();
     }
 }
